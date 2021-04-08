5d48a76681b044eb0a1af461179ce048d13cb794
Kai Magnus Müller <kai.mueller@hfg-gmuend.de>
2021-04-08 13:59:35 +0200

    added mobile collapsible nav

--- assets/src/main.js
+++ assets/src/main.js
@@ -249,3 +249,16 @@ window.addEventListener("keydown", function(event) {
             break;
     }
 })
+
+// HAMBURGER
+let hamburger = document.getElementById('hamburger');
+let nav = document.getElementById('nav');
+hamburger.addEventListener('click', function() {
+    if(hamburger.classList.contains('open')) {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+    } else {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+    }
+});
